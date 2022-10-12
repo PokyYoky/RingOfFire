d7594de8c9dc43e8b6cc4067914dde56ae0d3a54
PokyYoky <e-text@online.de>
2022-10-12 16:34:05 +0200

    new components: start-screen, button + scss files: variables, mixins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartScreenComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
