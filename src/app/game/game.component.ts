11af353042a157b6157bfffad6e737f55d0771b3
PokyYoky <e-text@online.de>
2022-10-14 17:15:15 +0200

    show played-cards 1

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -30,6 +30,7 @@ export class GameComponent implements OnInit {
       this.currentCard = this.game?.stack.pop();
       this.animationOnTakeCard = true;
       this.cardImage = '../../assets/img/cards/' + this.currentCard + '.png'; //'../../assets/img/cards/ace_1.png';
+      this.game?.playedCards.push(this.currentCard + '');
       //console.log(this.game?.stack);
     
       setTimeout(()=> {
