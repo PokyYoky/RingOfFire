e3c8e2918523baaab47cf887d12e77f283c2f1ed
PokyYoky <e-text@online.de>
2022-10-18 08:40:04 +0200

    get player automatically

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-player',
@@ -11,5 +11,7 @@ export class PlayerComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @Input() name: string = '';
 
 }
