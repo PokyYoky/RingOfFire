2897f211c158d2fd6ab95bd0d3d9ca1da4518489
PokyYoky <e-text@online.de>
2022-11-10 17:26:54 +0100

    Angularfire installed and integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,11 @@ import { FormsModule } from '@angular/forms';
 import {MatSelectModule} from '@angular/material/select';
 import { GameInfoComponent } from './game-info/game-info.component';
 import {MatCardModule} from '@angular/material/card';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -39,7 +44,11 @@ import {MatCardModule} from '@angular/material/card';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatCardModule
+    MatCardModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'ringoffire-ak',
+    appId: '1:201963370757:web:3583071ae6279edaaa5f6c',
+    storageBucket: 'ringoffire-ak.appspot.com',
+    apiKey: 'AIzaSyAlxCfJ9Ny4fd82t1Z7KjuFAWTHpP7XGnI',
+    authDomain: 'ringoffire-ak.firebaseapp.com',
+    messagingSenderId: '201963370757',
+  },
   production: true
 };
